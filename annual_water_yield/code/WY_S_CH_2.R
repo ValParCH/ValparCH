f45a737161cf44357b7893361a4a8dfc0126d130
ValParCH <79835132+ValParCH@users.noreply.github.com>
2022-03-02 09:56:08 +0100

    Add files via upload

---
+++ annual_water_yield/code/WY_S_CH_2.R
@@ -0,0 +1,31 @@
+library(raster)
+
+#-------------
+# This piece of code is solely to divide the invest wateryield output value by 10 to obtain actual mm/year
+# values, and then export it to the right folder.
+#-------------
+
+wd<-"C:/Users/kuelling/Documents/VALPAR/ES Assessment/Annual water yield/FINAL/SUPPLY/CODE"
+setwd(wd)
+
+fold<-"C:/Users/kuelling/Documents/VALPAR/ES Assessment/Annual water yield/FINAL/SUPPLY"
+
+proc<-function(year){
+  
+  name<-paste("wyield_",year,".tif",sep="")
+  
+  a<-raster(paste(wd,"INVEST",year,"output","per_pixel",name,sep="/"))
+  a<-a/10
+
+  name2<-paste("WY_S_CH_",year,".tif",sep="")
+  path<-paste(fold,year,name2,sep="/")
+
+  writeRaster(a,path)
+  
+  return(a)
+}
+
+
+wy_97<-proc("97")
+wy_09<-proc("09")
+wy_18<-proc("18")
